a95df1ecec1bd74c328f2f74623e007150dc0a0f
Dush5 <dushyant.tailor@gmail.com>
2021-11-09 21:02:59 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,19 +2,9 @@ from locust import HttpUser, constant, task
 
 
 class MyReqRes(HttpUser):
-    host = 'https://reqres.in'
+    host = 'http://ignite2.tp9249.df-tp.com'
     wait_time = constant(1)
 
     @task
     def get_users(self):
-        response = self.client.get("/api/users?page=2")
-        print(response.text)
-        print(response.status_code)
-
-
-    @task
-    def create_user(self):
-        response = self.client.post("/api/users", data={"name": "Dushyant", "job": "QA"})
-        print(response.text)
-        print(response.status_code)
-        print(response.headers)
+        response = self.client.get("/")
